357d8486a4a3b8838096e5378fe66fde574670a1
evolveris <20345716+evolveris@users.noreply.github.com>
2021-03-29 15:47:35 +0100

    implement circular cluster, add useeffect

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { useQuery, gql } from '@apollo/client';
 import ForceGraph3D from 'react-force-graph-3d';
+import { useEffect } from 'react';
 
 const ALL_PLANETS = gql`
 {
@@ -41,66 +42,69 @@ function App() {
 
   const { loading: loadingPlanets, data: planetData } = useQuery(ALL_PLANETS);
   const { loading: loadingFilms, data: filmData } = useQuery(ALL_FILMS);
-  
-  if (loadingPlanets || loadingFilms) return <p>Loading...</p>;
 
   const graphData = {
     nodes: [],
     links: []
   }
 
+  useEffect(() => {
+    
   const planetList = planetData?.allPlanets.planets;
   const filmList = filmData?.allFilms.films;
-
-  // populate with unique planets
-  for (let i = 0; i < planetList.length; i++) {
-    graphData["nodes"].push({
-      id: planetList[i]["id"],
-      name: planetList[i]["name"],
-      val: planetList[i]["diameter"] ? planetList[i]["diameter"] /1000 : 1
-    })
-  }
-
-  // populate with unique films
-  for (let i = 0; i < filmList.length; i++) {
-    graphData["nodes"].push({
-      id: filmList[i]["id"],
-      name: filmList[i]["title"],
-      val: 5
-    })
-  }
-
-  const filmlessPlanets = planetList.filter((obj) => obj["filmConnection"]["films"].length === 0);
-  // const filmPlanets = planetList.filter((obj) => obj["filmConnection"]["films"].length !== 0)
-
-  for (let i = 0; i < filmlessPlanets.length; i++) {
-    for (let j = 0; j < filmlessPlanets.length; j++) {
-      graphData["links"].push({
-        source: filmlessPlanets[i]["id"],
-        target: filmlessPlanets[j]["id"]
+  
+  if (planetList && filmList) {
+    // populate with unique planets
+    for (let i = 0; i < planetList.length; i++) {
+      graphData["nodes"].push({
+        id: planetList[i]["id"],
+        name: planetList[i]["name"],
+        val: planetList[i]["diameter"] ? planetList[i]["diameter"] /1000 : 1
       })
     }
 
-    // graphData["links"].push({
-    //   source: filmlessPlanets[i]["id"],
-    //   target: filmlessPlanets[j]["id"]
-    // })
-  }
+    // populate with unique films
+    for (let i = 0; i < filmList.length; i++) {
+      graphData["nodes"].push({
+        id: filmList[i]["id"],
+        name: filmList[i]["title"],
+        val: 5
+      })
+    }
 
-  // create tree with associations
-  for (let i = 0; i < planetList.length; i++) {
-    const filmPlanets = planetList[i]["filmConnection"]["films"];
-      if (filmPlanets.length > 0) {
-        for (let j = 0; j < filmPlanets.length; j++) {
-          graphData["links"].push({
-            source: planetList[i]["id"],
-            target: filmPlanets[j]["id"]
-          })
-        } 
+    const filmlessPlanets = planetList.filter((obj) => obj["filmConnection"]["films"].length === 0);
+
+    for (let i = 0; i < filmlessPlanets.length - 1; i++) {
+      if (i === filmlessPlanets.length - 2) {
+        graphData["links"].push({
+          source: filmlessPlanets[0]["id"],
+          target: filmlessPlanets[filmlessPlanets.length - 2]["id"]
+        })
+      } else {
+        graphData["links"].push({
+          source: filmlessPlanets[i]["id"],
+          target: filmlessPlanets[i+1]["id"]
+        })
       }
-  }  
+    }
 
-  console.log(graphData);
+    // create tree with associations
+    for (let i = 0; i < planetList.length; i++) {
+      const filmPlanets = planetList[i]["filmConnection"]["films"];
+        if (filmPlanets.length > 0) {
+          for (let j = 0; j < filmPlanets.length; j++) {
+            graphData["links"].push({
+              source: planetList[i]["id"],
+              target: filmPlanets[j]["id"]
+            })
+          } 
+        }
+    }
+  }
+    
+  }, [loadingFilms, loadingPlanets, filmData, planetData, graphData]);
+  
+  if (loadingPlanets && loadingFilms) return <p>Loading...</p>;
 
   return(<ForceGraph3D
     graphData={graphData}
